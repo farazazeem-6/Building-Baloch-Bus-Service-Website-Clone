b1f1105fa3e88411b6a617bd1e3c3eaaba3680c8
farazazeem-6 <faraz.tacticminds@gmail.com>
2025-07-11 10:41:54 +0500

    some changings

---
+++ login.js
@@ -0,0 +1,57 @@
+const Username = document.getElementById('username');
+const UserPassword = document.getElementById('password');
+
+
+function CheckValidationsForLogin() {
+    const GetUserName = Username.value.trim();
+    const GetUserPassword = UserPassword.value.trim();
+
+    let isValidLoginInput = true;
+
+    document.querySelector('.username-error').innerText = '';
+    document.querySelector('.password-error').innerText = '';
+
+    // const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,16}$/;
+
+    const emailRegex = /^[\w.-]+@[a-zA-Z_-]+?\.[a-zA-Z]{2,3}$/;
+    const usernameRegex = /^[a-zA-Z0-9._]{4,20}$/;
+
+
+
+    if (GetUserName === "") {
+        document.querySelector('.username-error').innerText = 'Username or email required';
+        isValidLoginInput = false;
+    } else if (!emailRegex.test(GetUserName) && !usernameRegex.test(GetUserName)) {
+        document.querySelector('.username-error').innerText = 'Invalid username or email';
+        isValidLoginInput = false;
+    }
+
+    if (GetUserPassword === "") {
+        document.querySelector('.password-error').innerText = 'Password required';
+        isValidLoginInput = false;
+    }
+
+    else if (GetUserPassword.length < 8) {
+        document.querySelector('.password-error').innerText = 'Password must be at least 8 characters';
+        isValidLoginInput = false;
+    }
+    else if (GetUserPassword.length > 20) {
+        document.querySelector('.password-error').innerText = 'Too long password (maximum-20)';
+        isValidLoginInput = false;
+    }
+
+
+
+    if (isValidLoginInput) {
+        alert('Logged in');
+        document.getElementById('form-1').reset();
+        document.getElementById('radio-form').reset();
+    }
+
+}
+Username.addEventListener('input', function () {
+    document.querySelector('.username-error').innerText = '';
+})
+UserPassword.addEventListener('input', function () {
+    document.querySelector('.password-error').innerText = '';
+})
